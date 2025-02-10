2a9af593c19768ca2de5211806ad79b8c4a9a2ec
David Dubins <d.dubins@utoronto.ca>
2025-02-10 14:38:17 -0500

    Update I2Cslave_struct.ino

--- I2C/I2Cslave_struct.ino
+++ I2C/I2Cslave_struct.ino
@@ -28,7 +28,7 @@ struct myStruct { // example structure to send over I2C. This was for a servo.
   bool b;   // bool is 1 byte
 };
 
-// Declare a union to help decode the structure once received or sent
+// Declare union to help represent myData as char array
 union myUnion { //declare union in global space
   char myCharArr[sizeof(myStruct)]; // char array to be shared with sData
   myStruct myData; //occupies same memory as myCharArr
